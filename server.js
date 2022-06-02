06db754ade557eb26424f57c5fa86700a2b97c24
RaghukumarTentu <tentu@leewayhertz.com>
2022-06-02 13:04:23 +0530

    modified

--- server.js
+++ server.js
@@ -18,31 +18,55 @@ app.get('/.well-known/stellar.toml', (req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("content-type", "text/plain");
     res.sendFile('stellar.toml', options);
-  })
+})
 
-  app.get('/sep24/info',(req,res)=>{
+app.get('/sep24/info',(req,res)=>{
+res.json({
+    "deposit": {
+        "AstroDollar": {
+            "enabled": true,
+            "fee_fixed": 1.0
+        }
+    },
+    "withdraw": {
+        "AstroDollar": {
+            "enabled": true,
+            "fee_fixed": 1.0
+        }
+    },
+    "fee": {
+        "enabled": true
+    },
+    "features": {
+        "account_creation": true,
+        "claimable_balances": true
+    }
+})
+})
+
+app.get('/sep6/info',(req,res)=>{
     res.json({
-      "deposit": {
-          "AstroDollar": {
-              "enabled": true,
-              "fee_fixed": 1.0
-          }
-      },
-      "withdraw": {
-          "AstroDollar": {
-              "enabled": true,
-              "fee_fixed": 1.0
-          }
-      },
-      "fee": {
-          "enabled": true
-      },
-      "features": {
-          "account_creation": true,
-          "claimable_balances": true
-      }
+        "deposit": {
+            "AstroDollar": {
+                "enabled": true,
+                "fee_fixed": 1.0
+            }
+        },
+        "withdraw": {
+            "AstroDollar": {
+                "enabled": true,
+                "fee_fixed": 1.0
+            }
+        },
+        "fee": {
+            "enabled": true
+        },
+        "features": {
+            "account_creation": true,
+            "claimable_balances": true
+        }
     })
-  })
+    })
 
 app.listen(port,()=>{
     console.log(`App is Running Locally on Port http://localhost:${port}`)
