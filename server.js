762b333ceabe54f8ad1d1ad2964dec3cd5a5b985
lhtRaghukumarTentu <tentu@leewayhertz.com>
2022-06-15 18:54:31 +0530

    modified

--- server.js
+++ server.js
@@ -222,6 +222,7 @@ app.get('/sep6/info',(req,res)=>{
 app.get('/auth',async(req, res) => {
     const clientPublicKey = req.query.account;
     const minTime = Math.floor(Date.now() / 1000);
+    console.log(minTime);
     const maxTime = minTime + CHALLENGE_EXPIRE_IN;
     const timebounds = {
       minTime: minTime.toString(),
@@ -234,14 +235,8 @@ app.get('/auth',async(req, res) => {
       });
     const account = new stellar.Account(SERVER_KEY_PAIR.publicKey(), INVALID_SEQUENCE);
     const tx = new stellar.TransactionBuilder(account, { timebounds,fee:100}).addOperation(op).setNetworkPassphrase(stellar.Networks.TESTNET).build()
-    // const time = 300;
-    // console.log(validateTimeBounds(tx,time));
-    // console.log(tx);
     tx.sign(SERVER_KEY_PAIR);
     res.json ({ transaction: tx.toEnvelope().toXDR("base64"), network_passphrase: stellar.Networks.TESTNET});
-    // const clientPublicKey = req.query.account;
-    // const challenge = stellar.Utils.buildChallengeTx( SERVER_KEY_PAIR, clientPublicKey, "SDF", 300, stellar.Networks.TESTNET, "stellartomlorg.herokuapp.com");
-    // res.json ({ transaction: challenge, network_passpharse: stellar.Networks.TESTNET });
 })
 
 
