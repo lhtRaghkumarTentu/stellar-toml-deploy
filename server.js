e99e1faffd9baad87d9b0f47c60ce20db0745783
RaghuKumarRk <rk25raghukumar@gmail.com>
2022-06-29 10:44:15 +0530

    Server Modified

--- server.js
+++ server.js
@@ -67,14 +67,7 @@ app.get('/.well-known/stellar.toml', (req, res, next) => {
     const account = new stellar.Account(SERVER_KEY_PAIR.publicKey(), INVALID_SEQUENCE);
     const transaction = new stellar.TransactionBuilder(account, { timebounds,fee:100}).addOperation(operation).setNetworkPassphrase(stellar.Networks.TESTNET).build()
     transaction.sign(SERVER_KEY_PAIR);
-    const { tx } = stellar.Utils.readChallengeTx(
-        transaction.toEnvelope().toXDR("base64"),
-        SERVER_KEY_PAIR.publicKey(),
-        stellar.Networks.TESTNET,
-        "stellartomlorg.herokuapp.com",
-        "stellartomlorg.herokuapp.com",
-      );
-    res.json ({ transaction: tx });
+    res.json ({ transaction: transaction.toEnvelope().toXDR("base64"), network_passphrase: stellar.Networks.TESTNET});
 })
 
 
